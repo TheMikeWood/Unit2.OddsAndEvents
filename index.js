0fab55dd8c5ac45f2d14f68e220ef6b701b22bf5
Michael Woodard <mr.m.woodard@gmail.com>
2025-01-16 09:26:28 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // TODO: this file! :)
+const numInput = document.getElementById("number");
+const form = document.querySelector("form");
+const numberBankOutput = document.querySelector("#numberBank output");
+const oddsOutput = document.querySelector("#odds output");
+const evensOutput = document.querySelector("#evens output");
+const sortOneButton = document.getElementById("sortOne");
+const sortAllButton = document.getElementById("sortAll");
+
+const numBank = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const number = parseInt(numInput.value, 10);
+  if (!isNaN(number)) {
+    numBank.push(number);
+    updateNumberBank();
+    numInput.value = "";
+  }
+});
+
+sortOneButton.addEventListener("click", () => {
+  if (numBank.length > 0) {
+    const number = numBank.shift();
+    sortNumber(number);
+    updateNumberBank();
+  }
+});
+
+sortAllButton.addEventListener("click", () => {
+  while (numBank.length > 0) {
+    const number = numBank.shift();
+    sortNumber(number);
+  }
+  updateNumberBank();
+});
+
+function updateNumberBank() {
+  numberBankOutput.textContent = numBank.join(", ");
+}
+
+function sortNumber(number) {
+  if (number % 2 === 0) {
+    evensOutput.textContent += `${number} `;
+  } else {
+    oddsOutput.textContent += `${number} `;
+  }
+}
